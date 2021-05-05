222023a42294a074d0d98a0e487c7339110d2bf5
Mateusz Motkowicz <mma.style113@gmail.com>
2021-05-05 13:06:55 +0200

    Login and create token JWT

--- WebAPI/Controllers/IdentityController.cs
+++ WebAPI/Controllers/IdentityController.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using WebAPI.Models;
@@ -59,6 +62,36 @@ namespace WebAPI.Controllers
             }
 
             return Ok(new Response { Succeeded = true, Message = "User created successfully!" });
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> LoginAsync(LoginModel login)
+        {
+            var user = await _userManager.FindByNameAsync(login.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, login.Password))
+            {
+                var authClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    expires: DateTime.Now.AddHours(2),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
         }
     }
 }
