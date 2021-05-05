cf97b06a33980dff00dc68ee72aced1a40b2f426
Mateusz Motkowicz <mma.style113@gmail.com>
2021-05-05 15:53:04 +0200

    Add new action

--- WebAPI/Controllers/PseudoProbableSequenceController.cs
+++ WebAPI/Controllers/PseudoProbableSequenceController.cs
@@ -2,20 +2,23 @@
 using Application.Interfaces;
 using Application.Models;
 using Application.Validators;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WebAPI.Attributes;
 using WebAPI.Wrappers;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/PseudoProbableSequence")]
+    [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PseudoProbableSequenceController : ControllerBase
     {
         private readonly IPseudoProbableSequenceService _pseudoProbableSequenceService;
@@ -26,11 +29,11 @@ namespace WebAPI.Controllers
         }
 
         [ValidateFilter]
-        [SwaggerOperation(Summary = "Get pseudo-probable sequences from 1 to 16")]
+        [SwaggerOperation(Summary = "Draw pseudo-probable sequences from 1 to 16")]
         [HttpGet("{Qty}")]
         public IActionResult Get([FromRoute] PseudoProbableSequenceQuantity quantity)
         {
-            var sequences = _pseudoProbableSequenceService.GetPseudoProbableSequences(quantity);
+            var sequences = _pseudoProbableSequenceService.DrawPseudoProbableSequences(quantity);
 
             return Ok(sequences);
         }
@@ -38,11 +41,21 @@ namespace WebAPI.Controllers
         [ValidateFilter]
         [SwaggerOperation(Summary = "Add pseudo-probable sequence")]
         [HttpPost]
-        public IActionResult Post([FromQuery] PseudoProbableSequenceDto dto)
+        public IActionResult Post(PseudoProbableSequenceDto dto)
         {
-            var sequences = _pseudoProbableSequenceService.PostPseudoProbableSequence(dto);
+            var sequences = _pseudoProbableSequenceService.PostPseudoProbableSequence(dto, User.FindFirstValue(ClaimTypes.NameIdentifier));
 
             return Created($"api/PseudoProbableSequence/{sequences.Id}", sequences);
+        }
+
+        //[ValidateFilter]
+        [SwaggerOperation(Summary = "Get pseudo-probable sequences")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var pseudoSequencesDto = _pseudoProbableSequenceService.GetPseudoProbableSequences();
+
+            return Ok(pseudoSequencesDto);
         }
     }
 }
